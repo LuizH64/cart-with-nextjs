9c0cd296199d908f03bb9b082b962d239f8ae2df
LuizH64 <60182096+LuizH64@users.noreply.github.com>
2022-06-08 17:21:23 -0300

    Changing basePath and assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: '/cart-with-nextjs',
+    assetPrefix: '/cart-with-nextjs',
     reactStrictMode: true,
     images: {
         loader: 'akamai',
