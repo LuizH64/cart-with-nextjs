1cad118b8842c62036d786c82e0e8aa9366fa11a
Luiz Rodrigues <luizhenriquealvesrodrigues64@gmail.com>
2022-06-08 19:25:42 -0300

    Corrigindo redirect

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
             {
                 source: '/',
                 destination: '/cart/small',
-                permanent: true,
+                basePath: false,
             },
         ]
     },
